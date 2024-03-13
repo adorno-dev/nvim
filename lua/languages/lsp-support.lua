b18af96bdb966325112611ff3149e8b979856282
adorno-dev <adorno.dev@gmail.com>
2024-03-13 19:14:38 -0300

    Improvements

--- lua/languages/lsp-support.lua
+++ lua/languages/lsp-support.lua
@@ -37,7 +37,7 @@ return {
 				type_definition = true,
 				enable_editorconfig_support = true,
 				enable_ms_build_load_projects_on_demand = false,
-				enable_import_completion = false,
+				enable_import_completion = true,
 				enable_roslyn_analyzers = true,
 				organize_imports_on_format = false,
 				sdk_include_prereleases = true,
