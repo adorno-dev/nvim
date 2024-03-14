6d0b5fd1e844efb44ba6558f34bb6b722751e354
adorno-dev <adorno.dev@gmail.com>
2024-03-14 09:28:21 -0300

    Bufferline offset

--- lua/components/bufferline.lua
+++ lua/components/bufferline.lua
@@ -1,12 +1,25 @@
 return {
-    'akinsho/bufferline.nvim', 
-    version = "*", 
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-        require ('bufferline')
-            .setup {}
-        vim.keymap.set('n', '<M-h>', ':bprevious<cr>', {})
-        vim.keymap.set('n', '<M-l>', ':bnext<cr>', {})
-        vim.keymap.set('n', '<C-w>', ':bprevious|bdelete #<cr>', {})
-    end
+	'akinsho/bufferline.nvim',
+	version = "*",
+	dependencies = 'nvim-tree/nvim-web-devicons',
+	config = function()
+		require('bufferline')
+			.setup {
+				options = {
+					mode = 'buffers',
+					offsets = {
+						{
+							filetype = 'NvimTree',
+							text = 'File Explorer',
+							highlight = 'Directory',
+							separator = true,
+							text_align = 'center'
+						},
+					}
+				},
+			}
+		vim.keymap.set('n', '<M-h>', ':bprevious<cr>', {})
+		vim.keymap.set('n', '<M-l>', ':bnext<cr>', {})
+		vim.keymap.set('n', '<C-w>', ':bprevious|bdelete #<cr>', {})
+	end
 }
--- lua/components/nvim-tree.lua
+++ lua/components/nvim-tree.lua
@@ -1,20 +1,20 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    dependencies = {
-        "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-        require("nvim-tree")
-            .setup {
-                view = {
-                    width = 50
-                },
-								filters = {
-										custom = {'bin','obj'}
-								}
-            }
-        vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<cr>', {})
-    end,
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree")
+			.setup {
+				view = {
+					width = 50
+				},
+				filters = {
+					custom = { 'bin', 'obj' }
+				}
+			}
+		vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<cr>', {})
+	end,
 }
